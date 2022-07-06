47d838d8df6b741e3f67e27f99f6d6031f68aff5
purnima-poo <np03a190173@herladcollege.edu.np>
2022-07-06 12:54:52 +0545

    feat: added navigation bar

--- bnb/lib/pages/home_page.dart
+++ bnb/lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -8,8 +9,55 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _page = 0;
+  final GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+      bottomNavigationBar: CurvedNavigationBar(
+        index: 0,
+        height: 60.0,
+        items: const [
+          Icon(
+            Icons.add,
+            size: 30,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.shopping_cart,
+            size: 30,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.favorite,
+            size: 30,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.home,
+            size: 30,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.person,
+            size: 30,
+            color: Colors.white,
+          ),
+        ],
+        color: Colors.pink.shade100,
+        buttonBackgroundColor: Colors.pink.shade100,
+        backgroundColor: Colors.white,
+        animationCurve: Curves.easeInOutCirc,
+        animationDuration: const Duration(milliseconds: 600),
+        onTap: (index) {
+          setState(() {
+            _page = index;
+          });
+        },
+        letIndexChange: (index) => true,
+      ),
+    );
   }
 }
